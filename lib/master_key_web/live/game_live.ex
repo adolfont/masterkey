7b833f828987f4ca4d86bff4fbda70e553573563
Cristine Guadelupe <cristineguadelupe@me.com>
2020-09-07 22:52:25 -0300

    GameLive - initial state

--- lib/master_key_web/live/game_live.ex
+++ lib/master_key_web/live/game_live.ex
@@ -1,8 +1,34 @@
 defmodule MasterKeyWeb.GameLive do
   use MasterKeyWeb, :live_view
 
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, params: nil)}
+  alias MasterKey.Game
+  alias MasterKeyWeb.GuessFormData
+
+  def mount(params, _session, socket) do
+    {:ok, build(socket, params)}
+  end
+
+  def build(socket, params) do
+    socket
+    |> board(params)
+    |> changeset(%{})
+    |> game
+  end
+
+
+  defp board(socket, %{"answer" => answer}) do
+    assign(socket, board: Game.new_board(answer))
+  end
+  defp board(socket, _) do
+    assign(socket, board: Game.new_board)
+  end
+
+  defp changeset(socket, params) do
+    assign(socket, changeset: GuessFormData.changeset(params))
+  end
+
+  defp game(socket) do
+    assign(socket, game: Game.to_map(socket.assigns.board))
   end
 
   def render(assigns) do
