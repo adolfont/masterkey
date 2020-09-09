a5ff5e21a6f2ed0b9e998be67178620549bbf25e
Cristine Guadelupe <cristineguadelupe@me.com>
2020-09-09 20:28:50 -0300

    Input validation for guess form data

--- lib/master_key_web/live/guess_form_data.ex
+++ lib/master_key_web/live/guess_form_data.ex
@@ -8,6 +8,8 @@ defmodule MasterKeyWeb.GuessFormData do
     |> Changeset.cast(params, Map.keys(@types))
     |> Changeset.validate_required([:guess])
     |> Changeset.validate_length(:guess, min: 4, max: 4)
+    |> Changeset.validate_format(:guess, ~r/^[1-8]{4}$/, message: "Guesses must be four numbers from 1-8" )
     |> Map.put(:action, :validate)
   end
+
 end
