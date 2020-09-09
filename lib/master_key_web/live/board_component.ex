a77e240b59060861777b16bd6318460abdbdd19f
Cristine Guadelupe <cristineguadelupe@me.com>
2020-09-09 03:50:03 -0300

    Almost there! Needs validation and small ui adjustments

--- lib/master_key_web/live/board_component.ex
+++ lib/master_key_web/live/board_component.ex
@@ -4,17 +4,36 @@ defmodule MasterKeyWeb.BoardComponent do
   def render(assigns) do
     ~L"""
     <%= for row <- @game.rows do %>
-      <div class="flex flex-row bg-gray-200">
+      <div class="flex flex-row bg-gray-100">
         <%= for guess <- row.guess do %>
-        <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
-          <%= guess %>
-        </div>
+          <div class="h-12 w-12 rounded-lg shadow-xs text-center px-4 py-2 m-2 <%= "bg-#{color(guess)}" %> ">
+            <p class="text-gray-100 opacity-90"> <%= guess %> <p>
+          </div>
         <% end %>
+        <div class="h-12 w-16 rounded-lg text-center px-4 py-2 m-2 bg-gray-100 flex flex-col flex-wrap">
+          <%= for i <- (0)..(row.score.reds), i > 0 do %>
+            <svg class="fill-current text-red-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
+            </svg>
+          <% end %>
+          <%= for i <- (0)..(row.score.whites), i > 0 do %>
+          <svg class="fill-current text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+            <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
+          </svg>
+          <% end %>
+        </div>
       </div>
     <% end %>
     """
   end
 
-
+  defp color(1), do: "pink-600"
+  defp color(2), do: "purple-600"
+  defp color(3), do: "yellow-500"
+  defp color(4), do: "green-600"
+  defp color(5), do: "blue-600"
+  defp color(6), do: "orange-600"
+  defp color(7), do: "red-600"
+  defp color(8), do: "gray-800"
 
 end
--- lib/master_key_web/live/game_live.ex
+++ lib/master_key_web/live/game_live.ex
@@ -37,16 +37,30 @@ defmodule MasterKeyWeb.GameLive do
     |> game
   end
 
-  def render(assigns) do
+  def render(%{game: %{status: :lost}} = assigns) do
     ~L"""
-    <h1>Welcome to MasterKey!</h1>
+    <button class="" phx-click="restart">Play again!</button>
+    """
+  end
+
+  def render(%{game: %{status: :win}} = assigns) do
+    ~L"""
+    <button class="" phx-click="restart">Play again!</button>
+    """
+  end
+
+  def render(%{game: %{status: :playing}} = assigns) do
+    ~L"""
+    <h1 class="text-3xl text-purple-700">Welcome to MasterKey!</h1>
     <%= live_component(@socket, BoardComponent, game: @game) %>
     <div id="guess">
     <%= form_for @changeset, "#", [as: :guess, phx_change: :validate, phx_submit: :guess], fn f -> %>
-      <label>
-        Guess: <%= text_input f, :guess %>
-      </label>
-      <%= error_tag f, :guess %>
+      <div class="field">
+        <label>Guess:
+          <%= text_input f, :guess %>
+        </label>
+        <%= error_tag f, :guess %>
+      </div>
       <%= submit "Submit", disabled: !@changeset.valid? %>
     <% end %>
     </div>
@@ -58,5 +72,8 @@ defmodule MasterKeyWeb.GameLive do
   end
   def handle_event("guess", %{"guess" => %{"guess" => params}}, socket) do
     {:noreply, guess(socket, params)}
+  end
+  def handle_event("restart", _params, socket) do
+    {:noreply, build(socket, %{})}
   end
 end
