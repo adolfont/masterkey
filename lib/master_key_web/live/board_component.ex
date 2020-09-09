5e3695548d23b08b7015b9ecbd9d7212af34a938
Cristine Guadelupe <cristineguadelupe@me.com>
2020-09-08 22:52:55 -0300

    BoardComponent - render the guesses

--- lib/master_key_web/live/board_component.ex
+++ lib/master_key_web/live/board_component.ex
@@ -3,25 +3,15 @@ defmodule MasterKeyWeb.BoardComponent do
 
   def render(assigns) do
     ~L"""
-    <div class="flex flex-row bg-gray-200">
-      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
-      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
-      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
-      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">4</div>
-    </div>
-    <div class="flex flex-row bg-gray-200">
-      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
-      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
-      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
-      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">4</div>
-    </div>
-
-    <%= inspect @game %>
-
     <%= for row <- @game.rows do %>
-      <p> <%= inspect row.guess %> </p>
+      <div class="flex flex-row bg-gray-200">
+        <%= for guess <- row.guess do %>
+        <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+          <%= guess %>
+        </div>
+        <% end %>
+      </div>
     <% end %>
-
     """
   end
 
