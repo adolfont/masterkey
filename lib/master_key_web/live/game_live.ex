eabdad70207d5b8582fa91dadb5cec39b6907c14
Cristine Guadelupe <cristineguadelupe@me.com>
2020-09-10 02:18:45 -0300

    Fix submit enabled/disabled

--- lib/master_key_web/live/game_live.ex
+++ lib/master_key_web/live/game_live.ex
@@ -13,8 +13,8 @@ defmodule MasterKeyWeb.GameLive do
     |> board(params)
     |> changeset(%{})
     |> game
+    |> tries
   end
-
 
   defp board(socket, %{"answer" => answer}) do
     assign(socket, board: Game.new_board(answer))
@@ -35,6 +35,15 @@ defmodule MasterKeyWeb.GameLive do
     socket
     |> assign(board: Game.guess(socket.assigns.board, guess))
     |> game
+    |> tries
+    |> changeset(%{})
+  end
+
+  defp tries(%{assigns: %{tries: tries}} = socket) when tries > 0 do
+    assign(socket, tries: tries - 1)
+  end
+  defp tries(socket) do
+    assign(socket, tries: 10)
   end
 
   def render(%{game: %{status: :lost}} = assigns) do
@@ -58,15 +67,18 @@ defmodule MasterKeyWeb.GameLive do
     <h1 class="text-3xl text-purple-700 mb-4 pb-4 border-b-2">Welcome to MasterKey!</h1>
     <%= live_component(@socket, BoardComponent, game: @game) %>
     <div id="guess">
-    <%= form_for @changeset, "#", [as: :guess, phx_change: :validate, phx_submit: :guess], fn f -> %>
-      <div class="field">
-        <label>Guess:
-          <%= text_input f, :guess, value: "" %>
-        </label>
-        <%= error_tag f, :guess %>
-      </div>
-      <%= submit "Submit", disabled: !@changeset.valid? %>
-    <% end %>
+      <%= form_for @changeset, "#", [as: :guess, phx_change: :validate, phx_submit: :guess], fn f -> %>
+        <div class="field">
+          <label>Guess:
+            <%= text_input f, :guess%>
+          </label>
+          <%= error_tag f, :guess %>
+        </div>
+        <%= submit "Submit", class: if !@changeset.valid?, do: "opacity-50 cursor-not-allowed" %>
+        <div class="shadow-xs text-center px-4 py-2 m-2 <%= "bg-#{color(@tries)}" %> ">
+          <p class="text-gray-100 opacity-90"> <%= @tries %> <p>
+        </div>
+      <% end %>
     </div>
     """
   end
@@ -74,10 +86,20 @@ defmodule MasterKeyWeb.GameLive do
   def handle_event("validate", %{"guess" => params}, socket) do
     {:noreply, changeset(socket, params)}
   end
+
+  def handle_event("guess", _params, %{assigns: %{changeset: %{valid?: false}}} = socket) do
+    {:noreply, socket}
+  end
   def handle_event("guess", %{"guess" => %{"guess" => params}}, socket) do
     {:noreply, guess(socket, params)}
   end
+
   def handle_event("restart", _params, socket) do
     {:noreply, build(socket, %{})}
   end
+
+  defp color(tries) when tries > 5, do: "green-600"
+  defp color(tries) when tries > 2, do: "yellow-500"
+  defp color(tries) when tries > 0, do: "red-600"
+
 end
